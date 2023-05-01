b39b7fe8018976ba13219db0abbe570d87c7c26e
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-05-01 14:31:06 +0900

    英語と日本語対応にした

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :books
+  get '/', to: redirect('/books')
+
+  scope "(:locale)", locale: /ja|en/ do
+    resources :books
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
