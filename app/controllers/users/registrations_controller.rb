0ebb91d5873809217b22f9a4b3bce52d4410147a
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-05-23 08:29:14 +0900

    ユーザ一覧ページを作成
    自分以外のユーザ情報は編集できないようにした

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,16 +38,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  protected
-
-  def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
-
-  def after_update_path_for(resource)
-    user_path(current_user)
-  end
-
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 Rails.application.routes.draw do
-  root to: 'books#index'
   resources :books
-
+  
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-
+  
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions#new'
     get 'users/sign_out', to: 'devise/sessions#destroy'
   end
-
+  
   resources :users
-  # get 'users/:id', to: 'users#show', as: 'show_user'
-  # get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
-  # patch 'users/:id', to: 'users#update', as: 'update_user'
-
+  root to: 'books#index'
 end
