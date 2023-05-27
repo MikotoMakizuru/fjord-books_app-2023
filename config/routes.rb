5488c3a3382de969a6ce2e75a1e46bc00e74b634
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-05-27 19:39:08 +0900

    ユーザ編集後のリダイレクト先をユーザ詳細ページに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   resources :books
   
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions#new'
     get 'users/sign_out', to: 'devise/sessions#destroy'
   end
-  
+
   resources :users
   root to: 'books#index'
 
