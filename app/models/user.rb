a46d4cdf47b1547bdc8c8d23ee64599e718686de
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-05-27 15:03:59 +0900

    郵便番号にバリデーションをかけた

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  validates :postcode, format: { with: /\A\d{3}[-]?\d{4}\z/, allow_blank: true }
+  validates :postcode, format: { without: /\s/, allow_nil: true }
 end
