f8017818f29082069dba14094da261d9f38cce3c
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-05-31 23:39:23 +0900

    reports コントローラを作成した

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'reports/index'
+  get 'reports/show'
+  get 'reports/new'
+  get 'reports/edit'
+  get 'reports/destroy'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
