f700d9478cd0a4686939843c6f250a0b03e75e4b
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-05-31 23:46:35 +0900

    reports の URL 定義に resources を使った書き方に修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'reports/index'
-  get 'reports/show'
-  get 'reports/new'
-  get 'reports/edit'
-  get 'reports/destroy'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
+  resources :reports
   resources :books
   resources :users, only: %i(index show)
 end
