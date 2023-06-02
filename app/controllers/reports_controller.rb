dbe2f4547b1a53aa36155769ee6e7c65df239657
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-06-03 01:53:26 +0900

    日報の一覧表示, 詳細表示, 作成, 編集, 削除をできるようにした

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,16 +1,52 @@
+# frozen_string_literal: true
+
 class ReportsController < ApplicationController
   def index
+    @reports = Report.order(:id).page(params[:page])
   end
 
   def show
+    @report = Report.find(params[:id])
   end
 
   def new
+    @report = Report.new
+  end
+
+  def create
+    @report = Report.new(report_params)
+
+    if @report.save
+      redirect_to @report, notice: '日報を保存しました'
+    else
+      render :new
+    end
   end
 
   def edit
+    @report = Report.find(params[:id])
+  end
+
+  def update
+    @report = Report.find(params[:id])
+
+    if @report.update(report_params)
+      redirect_to @report, notice: '日報を保存しました'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @report = Report.find(params[:id])
+    @report.destroy
+
+    redirect_to reports_url, notice: '本を削除しました'
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :body)
   end
 end
