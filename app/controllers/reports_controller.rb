1ab4dee7d75c1b76c99ec511bd10911b5f54a323
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-07-09 05:35:28 +0900

    i18n の対応漏れを修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,7 +19,7 @@ class ReportsController < ApplicationController
     @report = current_user.reports.new(report_params)
 
     if @report.save
-      redirect_to @report, notice: '日報を保存しました'
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new
     end
@@ -33,7 +33,7 @@ class ReportsController < ApplicationController
     @report = current_user.reports.find(params[:id])
 
     if @report.update(report_params)
-      redirect_to @report, notice: '日報を保存しました'
+      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit
     end
@@ -43,7 +43,7 @@ class ReportsController < ApplicationController
     @report = current_user.reports.find(params[:id])
     @report.destroy
 
-    redirect_to reports_url, notice: '日報を削除しました'
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
