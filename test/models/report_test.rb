2c0efbf3bef3f49a6767f68cffc8fb5c9c99d1c4
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-08-15 16:14:31 +0900

    Reportクラスのeditable?メソッドのテスト作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,14 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @alice = users(:alice)
+    @report_by_alice = reports(:report_by_alice)
+    @report_by_bob = reports(:report_by_bob)
+  end
+
+  test 'editable?' do
+    assert @report_by_alice.editable?(@alice)
+    assert_not @report_by_bob.editable?(@alice)
+  end
 end
