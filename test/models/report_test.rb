6211b9fe4ecd4fa651d2a2f7e3aa929314c72263
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-08-23 22:16:56 +0900

    reportモデルのメソッドのテスト作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -7,6 +7,7 @@ class ReportTest < ActiveSupport::TestCase
     @alice = users(:alice)
     @report_by_alice = reports(:report_by_alice)
     @report_by_bob = reports(:report_by_bob)
+    @report_by_carol = reports(:report_by_carol)
   end
 
   test 'editable?' do
@@ -16,5 +17,39 @@ class ReportTest < ActiveSupport::TestCase
 
   test 'created_on' do
     assert_equal Time.zone.today, @report_by_alice.created_on
+  end
+
+  test 'seve_mentions' do
+    # 言及なし
+    report = {
+      user: users(:alice),
+      title: 'alice title',
+      content: 'alice content'
+    }
+    report = Report.create(report)
+    assert_equal [], report.mentioning_reports
+
+    # 言及あり
+    report = {
+      user: users(:alice),
+      title: 'alice title',
+      content: "hoge http://localhost:3000/reports/#{@report_by_bob.id}"
+    }
+    report = Report.create(report)
+    assert_equal [@report_by_bob], report.mentioning_reports
+
+    # 言及するURLを更新
+    updated_content = {
+      content: "fuga http://localhost:3000/reports/#{@report_by_carol.id}"
+    }
+    report.update(updated_content)
+    assert_equal [@report_by_bob, @report_by_carol], report.mentioning_reports
+
+    # 言及するURLを削除して更新
+    deleted_mention_content = {
+      content: 'Dleted mention'
+    }
+    report.update(deleted_mention_content)
+    assert_equal [], report.reload.mentioning_reports
   end
 end
