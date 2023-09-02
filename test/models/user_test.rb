346e340d5d78ca33e9d05d199af580fb54cdc809
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-09-02 22:16:24 +0900

    ユーザ名をテストデータであることがわかるようにした

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,7 +7,7 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(email: 'foo@example.com', name: '')
     assert_equal 'foo@example.com', user.name_or_email
 
-    user = User.new(email: 'foo@example.com', name: 'foo')
-    assert_equal 'foo', user.name_or_email
+    user = User.new(email: 'foo@example.com', name: 'ふー')
+    assert_equal 'ふー', user.name_or_email
   end
 end
